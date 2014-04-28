b6c916a212fa485d02db90f66a57744be7ff48e8
Sondre Sallaup <sondre.sallaup@gmail.com>
2014-04-28 18:29:46 +0200

    a

--- www/js/camera.js
+++ www/js/camera.js
@@ -48,13 +48,11 @@ var app = {
     },
 
     takePicture: function() {
-        localStorage.setItem('imageURI', "../img/logo.png");
-          window.location = ('camera.html');
           /*document.getElementById("card").style.display = "block";
         $("div#picture").html('<font size="6"><i class="icon ion-loading-c"></i></font>');*/
       navigator.camera.getPicture( function( imageURI ) {
-          window.location = ('camera.html');
           localStorage.setItem('imageURI', imageURI);
+          window.location = ('camera.html');
        /* $("div#picture").html('<img src="'+ imageURI +'" class="img-rounded" height="350">');
             document.getElementById("button").style.display = "none";
           document.getElementById("card").style.display = "block";*/
